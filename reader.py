d325fa834fe2f6d312aff1a8dbe6794571fff338
Ludvig Lindholm <ludviglindholm127@gmail.com>
2022-03-17 11:18:53 +0100

    Did work to reader.py, have not tried it as the required data does not yet exist.

--- reader.py
+++ reader.py
@@ -1,6 +1,17 @@
+import csv
+import numpy as np
+file = "word_data.csv"
 
 
 def pairs():
     """Loads the data as a list of (id, word, frequency) tuples"""
 
-    return list()
+    def into_uint32(_id, word, frequency):
+        # uint32 should take up way less memory than python integers.
+        return np.uint32(_id), word, np.uint32(frequency)
+
+    with open(file, "r") as f:
+        reader = csv.reader(file)
+        data = list(reader)
+    data = [into_uint32(*d) for d in data]
+    return data
